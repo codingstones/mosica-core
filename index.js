c6f2f28a2d4ca9c6d8e13667b6e67ab9cd5bee7b
Dani Latorre <dani@danilat.com>
2016-10-30 19:56:28 +0100

    gets a gig by id

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 class GigService {
   constructor(httpClient) {
     this._httpClient = httpClient;
-    this._baseUrl = "http://www.mosica.es/api/1"
+    this._baseUrl = "http://www.mosica.es/api/1";
+    this._gigs_by_day = [];
   }
 
   retrieveNextGigs(){
     return new Promise((resolve, reject) => {
       this._httpClient.get(this._baseUrl + '/gigs').then((response) => {
-        resolve(response.body['response']);
+        this._gigs_by_day = response.body['response'];
+        resolve(this._gigs_by_day);
+      });
+    });
+  }
+
+  retrieveAGig(id){
+    return new Promise((resolve, reject) => {
+      this._gigs_by_day.forEach((day) => {
+        day.gigs.forEach((gig) => {
+          if(gig.id == id){
+            resolve(gig);
+            return;
+          }
+        });
       });
     });
   }
--- test/gig-service.spec.js
+++ test/gig-service.spec.js
@@ -14,24 +14,29 @@ describe('Gig service', () => {
     {id: 2, title: "celtas cortos"},
   ];
 
+  const someGigsByDay = [{
+    day: "Lunes putamiérdico",
+    gigs: someGigs
+  }];
+
   beforeEach(()=> {
     httpClient = new mosica.HttpClient();
     gigService = new mosica.GigService(httpClient);
 
     httpClientStub = sinon.stub(httpClient, 'get').returns({
       then: function(resp){
-        return resp({body: {response: someGigs}});
+        return resp({body: {response: someGigsByDay}});
       }
     });
   });
 
   context('retrieve the next gigs', () =>{
     it('gets the gigs', () => {
-      return gigService.retrieveNextGigs().then((gigs) => {
+      return gigService.retrieveNextGigs().then((gigsByDay) => {
+        let gigs = gigsByDay[0].gigs;
+
         expect(gigs.length).to.be.equal(2);
-
         expect(gigs[0].title).to.be.equal('dead broncos');
-
         expect(gigs[1].title).to.be.equal('celtas cortos');
       });
     });
@@ -43,5 +48,17 @@ describe('Gig service', () => {
     });
   });
 
-  
+  context('retrieve a gig', () => {
+    beforeEach((done)=> {
+      gigService.retrieveNextGigs().then(()=> {
+        done();
+      });
+    });
+
+    it('gets an existent gig', () => {
+      return gigService.retrieveAGig(1).then((gig) => {
+        expect(gig.title).to.be.equal('dead broncos');
+      });
+    });
+  });
 });
